f549e7880565c909769e73163eb407a62abaa681
hzwy23 <hzwy23@163.com>
2017-07-04 21:59:38 +0800

    update

--- admin/src/main/java/com/asofdate/AsofdateMain.java
+++ admin/src/main/java/com/asofdate/AsofdateMain.java
@@ -1,14 +1,14 @@
 package com.asofdate;
 
+import com.asofdate.batch.InitBatch;
 import com.asofdate.utils.Adaptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
-import com.asofdate.batch.InitBatch;
 
-@ComponentScan(basePackages = {"com.asofdate.utils", "com.asofdate.hauth","com.asofdate.batch"})
+@ComponentScan(basePackages = {"com.asofdate.utils", "com.asofdate.hauth", "com.asofdate.batch"})
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
