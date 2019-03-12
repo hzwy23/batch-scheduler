4d711ca51cf5f7b76d456ef306afce4a0046760f
hzwy23 <hzwy23@hzwy23s-MacBook-Pro.local>
2019-03-12 22:29:24 +0800

    修改接口

--- batch/src/main/java/com/asofdate/batch/controller/DispatchController.java
+++ batch/src/main/java/com/asofdate/batch/controller/DispatchController.java
@@ -47,45 +47,11 @@ public class DispatchController {
             log.info("batch id or domain jobKey is null ,batch jobKey is: {}, domain jobKey is: {}", batchId, domainId);
             return Hret.error(421, "domain_id is empty or batch_id is empty", null);
         }
-        return taskExecutorFeign.start(batchId,domainId);
-//
-//        if (BatchStatus.BATCH_STATUS_RUNNING == batchDefineService.getStatus(batchId)) {
-//            response.setStatus(421);
-//            logger.info("批次正在运行中，无法重新启动, 批次号是：{}", batchId);
-//            return Hret.error(421, "批次正在运行中", null);
-//        }
-//
-//        BatchRunConfDto bconf = batchDefineService.initConf(batchId, domainId);
-//        RetMsg retMsg = execService.init(bconf);
-//        if (!retMsg.checkCode()) {
-//            logger.info(retMsg.getMessage());
-//            response.setStatus(retMsg.getCode());
-//            return Hret.error(retMsg);
-//        }
-//
-//        retMsg = batchDefineService.runBatchInit(batchId);
-//        if (SysStatus.SUCCESS_CODE != retMsg.getCode()) {
-//            batchDefineService.setStatus(batchId, BatchStatus.BATCH_STATUS_ERROR);
-//            logger.info(retMsg.toString());
-//            response.setStatus(426);
-//            return Hret.error(retMsg);
-//        }
-//        logger.debug("初始化修改批次状态信息完成，批次号是：{}", batchId);
-//
-//        // 进度调度依赖关系管理
-//        // 根据依赖关系,开启任务触发器
-//        try {
-//            quartzSchedulerManager.createJobSchedulerService(bconf);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            batchDefineService.setStatus(batchId, BatchStatus.BATCH_STATUS_ERROR);
-//            return Hret.error(SysStatus.ERROR_CODE, "启动调度器失败", e.getMessage());
-//        }
-//        logger.debug("调度器创建成功，批次号是：{}", batchId);
-//
-//        quartzSchedulerManager.schedulerStart();
-//        logger.info("批次初始化完成，调度服务已启动，批次号是：{}", batchId);
-//        return Hret.success(SysStatus.SUCCESS_CODE, "start batch successfully. batch id is :" + batchId, null);
-
+        String retMsg =  taskExecutorFeign.start(batchId,domainId);
+        log.info("启动批次调度，返回信息是 ： {}", retMsg);
+        if (retMsg == null) {
+            return Hret.error(415,"启动批次调度服务失败", retMsg);
+        }
+        return Hret.success();
     }
 }
