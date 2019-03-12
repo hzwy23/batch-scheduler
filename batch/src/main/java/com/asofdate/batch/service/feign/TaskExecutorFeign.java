91605b9c7608d5b34896a2fad896911a9a71b5db
hzwy23 <hzwy23@hzwy23s-MacBook-Pro.local>
2019-03-12 23:27:19 +0800

    init

--- batch/src/main/java/com/asofdate/batch/service/feign/TaskExecutorFeign.java
+++ batch/src/main/java/com/asofdate/batch/service/feign/TaskExecutorFeign.java
@@ -1,7 +1,6 @@
 package com.asofdate.batch.service.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "task-executor")
 public interface TaskExecutorFeign {
 
-    @RequestMapping(value = "/batch/schedule/v1/dispatch/start", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/batch/schedule/v1/dispatch/start", method = RequestMethod.GET)
     String start(@RequestParam(value = "batchId") String batchId,
                  @RequestParam(value = "domainId") String domainId);
 }
